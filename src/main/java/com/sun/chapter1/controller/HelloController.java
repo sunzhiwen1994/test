6501d3c482bdcfa0b00bbe2b3298d24a3d335ae2
sunzhiwen1994 <31513018+sunzhiwen1994@users.noreply.github.com>
2018-11-12 19:37:44 +0800

    test

---
+++ src/main/java/com/sun/chapter1/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.sun.chapter1.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "hello world!";
+    }
+}
